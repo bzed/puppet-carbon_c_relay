93548ddb3f314103821042eb75785826f6b7c36b
Bernd Zeimetz <bernd@bzed.de>
2015-07-08 17:38:54 +0200

    Require systemd service.

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,14 +1,12 @@
 # carbon_c_relay::service
 class carbon_c_relay::service {
-    service { 'carbon-c-relay':
-        ensure     => running,
-        enable     => true,
-        hasstatus  => true,
-        hasrestart => true,
-        restart    => '/etc/init.d/relay reload',
-        require    => [ Concat[$carbon_c_relay::config_file],
-        File['/etc/init.d/relay'],
-        Package['carbon-c-relay']
+    service { 'carbon-c-relay.service':
+        ensure   => running,
+        enable   => true,
+        provider => systemd,
+        require  => [
+            Concat[$carbon_c_relay::config_file],
+            Package['carbon-c-relay']
         ],
     }
 }
